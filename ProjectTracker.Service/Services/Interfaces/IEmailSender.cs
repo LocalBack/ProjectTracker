c9b97638f9c1e98dadf0845e9fdc0700ebe05fff
Murat Görkem Kahya <63514252+LocalBack@users.noreply.github.com>
2025-07-30 11:02:12 +0300

    Add AutoMapper package and remove duplicate email sender

--- ProjectTracker.Service/Services/Interfaces/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Microsoft.AspNetCore.Identity.UI.Services
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string email, string subject, string htmlMessage);
-    }
-}
