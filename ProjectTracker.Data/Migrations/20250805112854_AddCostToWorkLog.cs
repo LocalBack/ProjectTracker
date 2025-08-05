d1769b8e7b79a32db568d2fcbdf49df795b176ec
DELL <muratgorkemkahya@gmail.com>
2025-08-05 14:43:32 +0300

    cost migration

---
+++ ProjectTracker.Data/Migrations/20250805112854_AddCostToWorkLog.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCostToWorkLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "WorkLogs",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "WorkLogs");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+    }
+}
